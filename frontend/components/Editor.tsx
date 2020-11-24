7323fc960eeae6eee2444e27337de09162485166
eslam-fakhry <eslamphakhry@gmail.com>
2020-11-25 01:07:53 +0200

    Add Editor component

---
+++ frontend/components/Editor.tsx
@@ -0,0 +1,23 @@
+import { Delta } from "quill";
+import React, { ReactElement, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+interface Props {}
+
+export default function Editor({}: Props): ReactElement {
+  const delta: Delta = {
+    ops: [{ insert: "\n" }],
+  } as Delta;
+  const [value, setValue] = useState(delta);
+
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={(_content, _delta, _, editor) => {
+        return setValue(editor.getContents());
+      }}
+    />
+  );
+}
