1bdac343412a08f7b40f85c2616feca6a402e356
eslam-fakhry <eslamphakhry@gmail.com>
2020-11-26 17:27:19 +0200

    Update Index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,20 +1,21 @@
-import { Badge, Box, Tag } from "@chakra-ui/react";
-import Tags from "../components/Tags";
+import dynamic from "next/dynamic";
 import Head from "next/head";
-import NoteCard from "../components/NoteCard";
-// import styles from '../styles/Home.module.css'
+import React from "react";
+import Panes from "../components/Panes";
 
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    // <div className={styles.container}>
-    <div>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>main</main>
-    </div>
+      <main>
+        <Panes />
+      </main>
+    </>
   );
 }
